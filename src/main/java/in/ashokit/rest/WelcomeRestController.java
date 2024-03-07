02336942b9c3824ce5367ef0ebd982f518d536a8
srdeekon <111860681+srdeekon@users.noreply.github.com>
2024-03-07 15:36:37 +0530

    Update WelcomeRestController.java

--- src/main/java/in/ashokit/rest/WelcomeRestController.java
+++ src/main/java/in/ashokit/rest/WelcomeRestController.java
@@ -18,7 +18,7 @@ public class WelcomeRestController {
 	@GetMapping
 	public String welcomeMsg() {
 		logger.info("***** welcomeMsg() execution start *****");
-		String msg = "Welcome to Ashok IT - Software Training Institute (HYD) ..!!";
+		String msg = "This is for testing spring boot application ..!!";
 		logger.info("***** welcomeMsg() execution end *****");
 		return msg;
 	}
